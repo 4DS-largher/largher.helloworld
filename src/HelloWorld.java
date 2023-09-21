85998f09daf97e41642882696469332f1f1b8433
4DS-largher <nicola.largher@buonarroti.tn.it>
2023-09-21 14:10:16 +0200

    feat: class that manages the greeting

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    private String word;
+
+    public HelloWorld(){
+        word = "Hello World!";
+    }
+
+    @Override
+    public String toString() {
+        return word;
+    }
+}
